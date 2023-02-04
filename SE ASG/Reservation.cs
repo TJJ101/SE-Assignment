13795b9f035b95953733c0981613cafb2a5938e7
Darius <s10207996@connect.np.edu.sg>
2023-02-04 17:04:35 +0800

    Initial test push

---
+++ SE ASG/Reservation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE_ASG
+{
+    public class Reservation
+    {
+        private string reservationID;
+        private string checkInDate;
+        private string checkOutDate;
+        private double revervationCost;
+        private bool cancelledReservation;
+
+
+        public void setCancelReservation()
+        {
+            // prompt for confirmation
+
+            //  get the curent date
+            // if check in date - current date >= 2: allow cancel. else dont allow
+
+            cancelledReservation = true;
+
+            // on successful transaction return credits back to customer 
+            // display successful/unsuccessful cancel message
+        }
+    }
+}
