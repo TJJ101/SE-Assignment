9c52b6b03b169ad4dccdfd2a1940bfb10f9eba29
Darius <s10207996@connect.np.edu.sg>
2023-02-09 13:23:34 +0800

    merge

--- SE ASG/Reservation.cs
+++ SE ASG/Reservation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,24 +9,58 @@ namespace SE_ASG
 {
     public class Reservation
     {
-        private string reservationID;
-        private string checkInDate;
-        private string checkOutDate;
-        private double revervationCost;
-        private bool cancelledReservation;
+        // changed the reservation ID to int (need change in class diagram)
+        public int reservationID { get; set; }
+        public DateTime checkInDate { get; set; }
+        public DateTime checkOutDate { get; set; }
+        public double revervationCost { get; set; }
+        public bool cancelledReservation { get; set; }
+        public double reservationCost { get; set; }    
 
+        //need to add this payment made into class diagram
+        public bool paymentMade { get; set; }
 
-        public void setCancelReservation()
+        private Guest guest;
+
+        //need add this into the class diagra,
+        public Room room;
+
+        public Guest Guest
         {
-            // prompt for confirmation
+            set
+            {
+                if (guest != value)
+                {
+                    guest = value;               
+                    value.ReserveHotel(this);
+                }
+            }
+        }
 
-            //  get the curent date
-            // if check in date - current date >= 2: allow cancel. else dont allow
+        public bool setCancelReservation()
+        {
+            Console.WriteLine("Are you sure you want to cancel? (y/n)");
+            string answer = Console.ReadLine();
 
-            cancelledReservation = true;
+            DateTime now = DateTime.Now;
 
-            // on successful transaction return credits back to customer 
-            // display successful/unsuccessful cancel message
+            if (answer == "y" && (checkInDate - now).TotalDays >= 2 && cancelledReservation == false)
+            {
+                cancelledReservation = true;
+                guest.balance += reservationCost;
+                room.availability = true;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // add this into class diagram
+        public void displayDetails()
+        {
+            Console.WriteLine("\n-------- Details --------\nRervation ID: "+ reservationID +"\nCheck in date: "+ checkInDate +"\nCheck out date: "+ checkOutDate +"\nIs cancelled: "+ cancelledReservation +"\nPayment made: "+ paymentMade +"");
         }
     }
 }
